27f0091b2b2f774b70ed11f4c08469fad19c5c0e
inventory69 <admin@sad.run>
2025-10-05 22:49:29 +0200

    Remove Discord bot config files from .gitignore and add initial Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+import discord
+
+CommandPrefix = "!"
+BotName = "Haze World Bot"
+Intents = discord.Intents.default()
+Intents.members = True
+Intents.message_content = True
+PINK = discord.Color(0xFF69B4)  # Hot Pink für Embeds
